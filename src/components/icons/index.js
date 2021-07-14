af53ea91f93fa339da402fefce8f6175622482e7
Kübra <kueben96@gmail.com>
2021-07-14 11:23:04 +0300

    alle funcs without edit

---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faTasks, faTrashAlt, faCheckCircle, faThList, faSmileBeam, faEdit } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCoffee, faTasks, faTrashAlt, faCheckCircle, faThList, faSmileBeam, faEdit)
+
